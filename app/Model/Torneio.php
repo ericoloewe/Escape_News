d65fea1b153f32ce0cac811637266c76e2a97fbd
ericoloewe <ed-lowe@hotmail.com>
2015-03-13 17:12:53 -0300

    Torneio

---
+++ app/Model/Torneio.php
@@ -0,0 +1,29 @@
+<?php
+
+class Torneio extends AppModel {
+    public $name = 'Torneio';
+    public $useTable = 'torneios';
+
+    public $validate = array(
+        'nome' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A Name is required'
+            )
+        ),                
+        'data' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A email is required'
+            )
+        ),                
+        'cash' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A email is required'
+            )
+        )
+    );
+}
+
+?>
